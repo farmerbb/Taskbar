e4e7e6f68d9c594ab6964819c1a1121759ba8715
Braden Farmer <farmerbb@gmail.com>
2016-12-04 12:08:04 -0700

    Remove unused imports

--- app/src/main/java/com/farmerbb/taskbar/activity/HomeActivity.java
+++ app/src/main/java/com/farmerbb/taskbar/activity/HomeActivity.java
@@ -26,7 +26,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -39,7 +38,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 
-import com.farmerbb.taskbar.BuildConfig;
 import com.farmerbb.taskbar.R;
 import com.farmerbb.taskbar.service.NotificationService;
 import com.farmerbb.taskbar.service.StartMenuService;
