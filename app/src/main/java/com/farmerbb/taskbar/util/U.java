b3cfea4dc8995fd4634befd72c965031947986a5
Braden Farmer <farmerbb@gmail.com>
2019-04-04 21:36:09 -0600

    Partially revert previous commit

--- app/src/main/java/com/farmerbb/taskbar/util/U.java
+++ app/src/main/java/com/farmerbb/taskbar/util/U.java
@@ -1240,36 +1240,29 @@ public class U {
         }
 
         // Customizations for BlissOS
-        if(isBlissOs(context)) {
-            if(getCurrentApiVersion() >= 28.0f) {
-                try {
-                    Settings.System.putString(context.getContentResolver(), "navigation_bar_show", "null");
-                } catch (Exception e) { /* Gracefully fail */ }
+        if(isBlissOs(context) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && !pref.getBoolean("bliss_os_prefs", false)) {
+            SharedPreferences.Editor editor = pref.edit();
+
+            if(hasFreeformSupport(context)) {
+                editor.putBoolean("freeform_hack", true);
             }
 
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
-                    && !pref.getBoolean("bliss_os_prefs", false)) {
-                SharedPreferences.Editor editor = pref.edit();
-
-                if(hasFreeformSupport(context))
-                    editor.putBoolean("freeform_hack", true);
-
-                editor.putString("recents_amount", "running_apps_only");
-                editor.putString("refresh_frequency", "0");
-                editor.putString("max_num_of_recents", "2147483647");
-                editor.putString("sort_order", "true");
-                editor.putString("window_size", "phone_size");
-                editor.putBoolean("full_length", true);
-                editor.putBoolean("dashboard", true);
-                editor.putBoolean("app_drawer_icon", true);
-                editor.putBoolean("button_back", true);
-                editor.putBoolean("button_home", true);
-                editor.putBoolean("button_recents", true);
-                editor.putBoolean("auto_hide_navbar", true);
-                // editor.putBoolean("shortcut_icon", false);
-                editor.putBoolean("bliss_os_prefs", true);
-                editor.apply();
-            }
+            editor.putString("recents_amount", "running_apps_only");
+            editor.putString("refresh_frequency", "0");
+            editor.putString("max_num_of_recents", "2147483647");
+            editor.putString("sort_order", "true");
+            editor.putString("window_size", "phone_size");
+            editor.putBoolean("full_length", true);
+            editor.putBoolean("dashboard", true);
+            editor.putBoolean("app_drawer_icon", true);
+            editor.putBoolean("button_back", true);
+            editor.putBoolean("button_home", true);
+            editor.putBoolean("button_recents", true);
+            editor.putBoolean("auto_hide_navbar", true);
+         // editor.putBoolean("shortcut_icon", false);
+            editor.putBoolean("bliss_os_prefs", true);
+            editor.apply();
         }
 
         // Customizations for Android-x86 devices (non-Bliss)
