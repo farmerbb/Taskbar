fa5863684396093d425c30d25668185161adb8f0
Braden Farmer <farmerbb@gmail.com>
2019-11-07 23:09:43 -0700

    Revert "Switch to LOCAL_STATIC_ANDROID_LIBRARIES"
    This reverts commit 7e41156819215733176f14c4f05660c18a22182a.

--- Android.mk
+++ Android.mk
@@ -1,12 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+appcompat_dir := frameworks/support/v7/appcompat
 res_dir := app/src/main/res
 nonlib_res_dir := app/src/nonlib/res
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/$(res_dir) $(LOCAL_PATH)/$(nonlib_res_dir)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/$(res_dir) $(LOCAL_PATH)/$(nonlib_res_dir) $(appcompat_dir)/res
 
 LOCAL_SRC_FILES := \
         $(call all-java-files-under, app/src/main/java) \
@@ -17,7 +18,7 @@ LOCAL_SRC_FILES := \
 
 LOCAL_MANIFEST_FILE := app/src/androidx86/AndroidManifest.xml
 
-LOCAL_STATIC_ANDROID_LIBRARIES := \
+LOCAL_STATIC_JAVA_LIBRARIES := \
         android-support-v4 \
         android-support-v7-appcompat \
         android-support-design
@@ -34,6 +35,8 @@ LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_AAPT_FLAGS := \
         --auto-add-overlay \
-        --rename-manifest-package com.farmerbb.taskbar.androidx86
+        --rename-manifest-package com.farmerbb.taskbar.androidx86 \
+        --extra-packages android.support.v7.appcompat \
+        --extra-packages android.support.design
 
 include $(BUILD_PACKAGE)
