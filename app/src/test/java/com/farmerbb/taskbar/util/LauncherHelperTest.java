21e454e5dc1265bc772e9d704e5a624aee019893
utzcoz <utzcoz@outlook.com>
2020-04-13 11:42:28 +0800

    Add unit tests for LauncherHelper and MenuHelper
    Test: run ./gradlew test, and all tests passed.
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>

---
+++ app/src/test/java/com/farmerbb/taskbar/util/LauncherHelperTest.java
@@ -0,0 +1,47 @@
+package com.farmerbb.taskbar.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
+public class LauncherHelperTest {
+    private LauncherHelper launcherHelper = LauncherHelper.getInstance();
+
+    @Test
+    public void testGetInstance() {
+        assertNotNull(launcherHelper);
+        for (int i = 1; i <= 20; i++) {
+            assertEquals(launcherHelper, LauncherHelper.getInstance());
+        }
+    }
+
+    @Test
+    public void testIsHomeScreen() {
+        assertFalse(launcherHelper.isOnHomeScreen());
+        launcherHelper.setOnPrimaryHomeScreen(true);
+        assertTrue(launcherHelper.isOnHomeScreen());
+        launcherHelper.setOnPrimaryHomeScreen(false);
+        launcherHelper.setOnSecondaryHomeScreen(true, 1);
+        assertTrue(launcherHelper.isOnHomeScreen());
+        launcherHelper.setOnSecondaryHomeScreen(false, 1);
+        assertEquals(-1, launcherHelper.getSecondaryDisplayId());
+    }
+
+    @Test
+    public void testIsOnSecondaryHomeScreen() {
+        assertFalse(launcherHelper.isOnSecondaryHomeScreen());
+        launcherHelper.setOnPrimaryHomeScreen(true);
+        assertFalse(launcherHelper.isOnSecondaryHomeScreen());
+        launcherHelper.setOnSecondaryHomeScreen(true, 1);
+        assertTrue(launcherHelper.isOnSecondaryHomeScreen());
+        launcherHelper.setOnPrimaryHomeScreen(false);
+        assertTrue(launcherHelper.isOnSecondaryHomeScreen());
+        launcherHelper.setOnSecondaryHomeScreen(false, 1);
+    }
+}
---
+++ app/src/test/java/com/farmerbb/taskbar/util/MenuHelperTest.java
@@ -0,0 +1,39 @@
+package com.farmerbb.taskbar.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
+public class MenuHelperTest {
+    private MenuHelper menuHelper = MenuHelper.getInstance();
+
+    @Test
+    public void testGetInstance() {
+        assertNotNull(menuHelper);
+        for (int i = 1; i <= 20; i++) {
+            assertEquals(menuHelper, MenuHelper.getInstance());
+        }
+    }
+
+    @Test
+    public void testSetStartMenuOpen() {
+        assertFalse(menuHelper.isStartMenuOpen());
+        menuHelper.setStartMenuOpen(true);
+        assertTrue(menuHelper.isStartMenuOpen());
+        menuHelper.setStartMenuOpen(false);
+    }
+
+    @Test
+    public void testSetContextMenuOpen() {
+        assertFalse(menuHelper.isContextMenuOpen());
+        menuHelper.setContextMenuOpen(true);
+        assertTrue(menuHelper.isContextMenuOpen());
+        menuHelper.setContextMenuOpen(false);
+    }
+}
