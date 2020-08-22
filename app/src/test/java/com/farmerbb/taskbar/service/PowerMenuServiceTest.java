603b726cdc3fec59f3544b96db2da99d759e0fb7
utzcoz <utzcoz@outlook.com>
2020-08-22 18:06:32 +0800

    Add test for PowerMenuService
    Test: ./gradlew test
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>

---
+++ app/src/test/java/com/farmerbb/taskbar/service/PowerMenuServiceTest.java
@@ -0,0 +1,30 @@
+package com.farmerbb.taskbar.service;
+
+import android.content.Intent;
+
+import com.farmerbb.taskbar.util.U;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowAccessibilityService;
+
+import static com.farmerbb.taskbar.util.Constants.ACTION_ACCESSIBILITY_ACTION;
+import static com.farmerbb.taskbar.util.Constants.EXTRA_ACTION;
+import static org.junit.Assert.assertTrue;
+import static org.robolectric.Shadows.shadowOf;
+
+@RunWith(RobolectricTestRunner.class)
+public class PowerMenuServiceTest {
+    @Test
+    public void testAccessibilityActionPerforming() {
+        PowerMenuService service = Robolectric.setupService(PowerMenuService.class);
+        Intent intent = new Intent(ACTION_ACCESSIBILITY_ACTION);
+        int testAction = 1000;
+        intent.putExtra(EXTRA_ACTION, testAction);
+        U.sendBroadcast(service, intent);
+        ShadowAccessibilityService shadowService = shadowOf(service);
+        assertTrue(shadowService.getGlobalActionsPerformed().contains(testAction));
+    }
+}
